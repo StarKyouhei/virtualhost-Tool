00d715adc168f2b52c385f885db28ad589c4c472
kyohei <kyoheistar08@gmail.com>
2016-07-02 17:38:30 +0900

    [Modify] tempalteクラスのファイル位置を変更

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 from config import bootstrap
 if __name__ == "__main__":
     # 読み込むディレクトリを登録 ライブラリー
-    bootstrap.setSysPathAppend('lib')
+    bootstrap.setSysPathAppend('lib/Template')
 
     import TemplateApache
     tet = TemplateApache.TemplateApache()
